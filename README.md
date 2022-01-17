349b6571454bf753683a4f8295ced88405708cce
brittonashford <brittonashford@users.noreply.github.com>
2022-01-17 12:55:11 -0600

    working final draft

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # portfolio
 Express/Node app to share projects from my current portfolio
+
+To run this locally:
+1. Open Visual Studio Code (or another text editor of your choosing)
+2. Go to the terminal and enter "npm install" to install the required packages
+3. Debug using node.js
+4. enter "localhost:3000" in your browser
--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 const express = require('express');
 const app = express();
-/*const path = '';*/
 const data = require('./data.json')
 
 app.set('view engine', 'pug');
 
+//Static HTML, CSS, and Images
 app.use('/static', express.static('public'));
 
+//path for home page
 app.get('/', (req, res) => {
     res.render('index', { data });
 });
 
-
+//path for about page
 app.get('/about', (req, res) => {
+    res.status=500;
+    res.sendStatus(500);
     res.render('about');
 })
 
 
+//retrieves project or a 404 error
 app.get('/projects/:id', (req, res, next) => {
     const { id } = req.params;
     const proj = data.projects[id];
 
     if(data.projects[id]){
         res.render('project', { proj })
-    } else {
-        const err = new Error('Project does not exist.');
+    } else {  //if user types a number in the url that doesn't correspond to a project
+        const err = new Error('Project does not exist (404).');
         err.status = 404;
         next(err);
     }  
+})
+
+//server error/default error handler
+app.use((err, req, res, next) => {
+        err.status = 500;
+        err.message = 'An error occurred while processing your request (500).'
+        console.log(err.message, err.status);
+        next(err);
 })
 
 
